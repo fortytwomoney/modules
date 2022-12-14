40d60c556bf7c9143c4d9076c5373ac2e33ec7ab
Maurits Bos <mb.brains@gmail.com>
2022-12-14 13:38:47 +0100

    added some todos to zap

--- contracts/autocompounder/src/handlers/execute.rs
+++ contracts/autocompounder/src/handlers/execute.rs
@@ -1,4 +1,5 @@
 use cosmwasm_std::{DepsMut, Env, MessageInfo, Uint128};
+use cw_asset::Asset;
 use forty_two::autocompounder::AutocompounderExecuteMsg;
 
 use crate::contract::{AutocompounderApp, AutocompounderResult};
@@ -13,9 +14,12 @@ pub fn execute_handler(
     msg: AutocompounderExecuteMsg,
 ) -> AutocompounderResult {
     match msg {
-        AutocompounderExecuteMsg::UpdateFeeConfig { performance, withdrawal, deposit } => {
-            update_fee_config(deps, info, app, performance, withdrawal, deposit)
-        }
+        AutocompounderExecuteMsg::UpdateFeeConfig {
+            performance,
+            withdrawal,
+            deposit,
+        } => update_fee_config(deps, info, app, performance, withdrawal, deposit),
+        AutocompounderExecuteMsg::Zap { pool, funds } => zap(deps, info, _env, app, pool, funds),
         _ => Err(AutocompounderError::ExceededMaxCount {}),
     }
 }
@@ -30,6 +34,26 @@ pub fn update_fee_config(
     _deposit: Option<Uint128>,
 ) -> AutocompounderResult {
     dapp.admin.assert_admin(deps.as_ref(), &msg_info.sender)?;
+
+    unimplemented!()
+}
+
+// im assuming that this is the function that will be called when the user wants to pool AND stake their funds
+pub fn zap(
+    deps: DepsMut,
+    msg_info: MessageInfo,
+    env: Env,
+    dapp: AutocompounderApp,
+    pool: String,
+    funds: Asset,
+) -> AutocompounderResult {
+    // TODO: Check if the pool is valid
+
+    // TODO: Swap the funds into 50/50. Might not be nescesarry with dex module single sided add liquidity
+
+    // TODO: get the liquidity token amount
+
+    // TODO: stake the liquidity token
 
     unimplemented!()
 }
