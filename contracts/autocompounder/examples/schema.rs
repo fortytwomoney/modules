94032d86ec12140c8c10ea2f54d0d92dd84a3f6e
adairrr <32375605+adairrr@users.noreply.github.com>
2023-01-11 22:13:24 +0100

    Comment out schema gen for autocompounder due to bug

--- contracts/autocompounder/examples/schema.rs
+++ contracts/autocompounder/examples/schema.rs
@@ -1,8 +1,8 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use autocompounder::contract::AutocompounderApp;
 use cosmwasm_schema::remove_schemas;
+// use autocompounder::contract::AutocompounderApp;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -10,5 +10,6 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    AutocompounderApp::export_schema(&out_dir);
+    // TODO: figure out issue with feature
+    // AutocompounderApp::export_schema(&out_dir);
 }
