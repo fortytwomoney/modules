05d38a853a569a602a76443e6d937d5517e54e07
cyberhoward <cyberhoward@protonmail.com>
2023-01-13 14:50:30 +0100

    fix import

--- contracts/autocompounder/examples/schema.rs
+++ contracts/autocompounder/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::remove_schemas;
-// use autocompounder::contract::AutocompounderApp;
+use autocompounder::contract::AutocompounderApp;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -11,5 +11,5 @@ fn main() {
     remove_schemas(&out_dir).unwrap();
 
     // TODO: figure out issue with feature
-    // AutocompounderApp::export_schema(&out_dir);
+    AutocompounderApp::export_schema(&out_dir);
 }
