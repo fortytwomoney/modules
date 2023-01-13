3019c19ed5965a65f1103b15194f8e3845ba6509
cyberhoward <cyberhoward@protonmail.com>
2023-01-13 16:50:41 +0100

    implement config query

--- contracts/autocompounder/src/handlers/query.rs
+++ contracts/autocompounder/src/handlers/query.rs
@@ -1,7 +1,7 @@
 use crate::contract::AutocompounderApp;
 use crate::state::{Config, CONFIG};
 use cosmwasm_std::{to_binary, Binary, Deps, Env, StdResult};
-use forty_two::autocompounder::AutocompounderQueryMsg;
+use forty_two::autocompounder::{AutocompounderQueryMsg, ConfigResponse};
 
 const _DEFAULT_PAGE_SIZE: u8 = 5;
 const _MAX_PAGE_SIZE: u8 = 20;
@@ -14,14 +14,23 @@ pub fn query_handler(
     msg: AutocompounderQueryMsg,
 ) -> StdResult<Binary> {
     match msg {
-        AutocompounderQueryMsg::Config {} => to_binary(&query_config(deps)?)?,
-    };
-    unimplemented!();
+        AutocompounderQueryMsg::Config {} => to_binary(&query_config(deps)?)
+    }
 }
 
 /// Returns the current configuration.
-pub fn query_config(deps: Deps) -> StdResult<Config> {
-    let _config = CONFIG.load(deps.storage)?;
-
-    unimplemented!();
+pub fn query_config(deps: Deps) -> StdResult<ConfigResponse> {
+    let config = CONFIG.load(deps.storage)?;
+    // crate ConfigResponse from config
+    Ok(ConfigResponse {
+        staking_contract: config.staking_contract,
+        pool_address: config.pool_address,
+        pool_data: config.pool_data,
+        liquidity_token: config.liquidity_token,
+        vault_token: config.vault_token,
+        commission_addr: config.commission_addr,
+        fees: config.fees,
+        bonding_period: config.bonding_period,
+        min_unbonding_cooldown: config.min_unbonding_cooldown,
+    })
 }
